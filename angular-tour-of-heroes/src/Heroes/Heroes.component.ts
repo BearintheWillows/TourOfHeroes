21bf0765c68946899dc1d1939b97ea173a1adc56
Stuart Willows <SJWillows91@outlook.com>
2022-12-19 19:54:34 +0000

    Displayed and Styled list of Heroes and Details section

--- angular-tour-of-heroes/src/Heroes/Heroes.component.ts
+++ angular-tour-of-heroes/src/Heroes/Heroes.component.ts
@@ -1,21 +1,26 @@
-import { Hero } from './../app/hero';
+import { Hero } from '../app/hero';
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-Heroes',
+  selector: 'app-heroes',
   templateUrl: './Heroes.component.html',
   styleUrls: ['./Heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes: Hero[] = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
 }
+
--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from '../Heroes/Heroes.component';
+import { HeroesComponent } from '../heroes/Heroes.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
